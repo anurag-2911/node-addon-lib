f5787b95157668781f757dda6e909deef902c822
Anurag Kumar <anurag.kumar@microfocus.com>
2021-04-13 15:53:16 +0530

    simple hello world example of node addon using NAPI.

---
+++ src/lib/helloworldexample.cpp
@@ -0,0 +1,16 @@
+#include "helloworldexample.h"
+
+std::string helloworldexample::hello(){
+    return "Hello World";
+}
+
+Napi::String helloworldexample::HelloWrapped(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::String returnValue = Napi::String::New(env, helloworldexample::hello());
+    return returnValue;
+}
+
+Napi::Object helloworldexample::Init(Napi::Env env, Napi::Object exports) {
+    exports.Set("hello", Napi::Function::New(env, helloworldexample::HelloWrapped));
+    return exports;
+}
